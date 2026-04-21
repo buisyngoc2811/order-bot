fae6d514f324f07763d343f08ee2b3ac7a253ef9
buisyngoc2811 <142831019+buisyngoc2811@users.noreply.github.com>
2026-04-22 02:45:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Bot is alive");
 });
-
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log("Web server chạy");
 });
@@ -26,6 +24,15 @@ const {
   TextInputStyle,
   WebhookClient
 } = require("discord.js");
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log("Bot đã online!");
+});
+
+client.login(process.env.TOKEN);
 
 // ===== PRICE =====
 const PRICE_LIST = {
